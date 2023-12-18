7772509f782c97a6b47b92b9bfcbc6f9edac6eb6
Diego Lopez <diegolopezv1094@gmail.com>
2023-12-18 12:14:26 -0500

    Init proyect

---
+++ Porfolio-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navegation from './components/Navegation/Navegation'
+
+
+function App() {
+
+  return (
+    <>
+      <Navegation />
+    </>
+  )
+}
+
+export default App
---
+++ Porfolio-react/src/components/Navegation/Navegation.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled"
+
+const Aside = styled.div`
+    background-color: yellow;
+
+`
+
+
+
+
+function Navegation() {
+
+    return (
+        <>
+            <Aside>Hello WORLD</Aside>
+
+        </>
+    )
+
+}
+
+export default Navegation;
